7a56bf925c9ecb5cb4d8ce893e7db6a39ad88ef1
Mohamed <mohamederwa.11@gmail.com>
2023-02-16 14:50:35 -0500

    mongodb connection

--- config/app.js
+++ config/app.js
@@ -10,21 +10,36 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// Database Setup
+let mongoose = require("mongoose");
+let DB = require("./db")
+
+// Point mongoose to DB URI
+mongoose.set('strictQuery', false);
+mongoose.connect(DB.URI, {useNewUrlParser: true, UseUnifiedTopology: true});
+
+let mongodb = mongoose.connection;
+mongodb.on('error', console.error.bind(console, 'Connection Error: '));
+mongodb.once('open', () => {
+  console.log('Connected to MongoDB...');
+
+})
+
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs'); // express  -e
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
