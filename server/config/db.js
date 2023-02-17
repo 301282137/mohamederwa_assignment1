16519b126c91a88e577df15c19153c17041625e8
Mohamed <mohamederwa.11@gmail.com>
2023-02-17 16:00:04 -0500

    Added remote DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost:27017/comp229_sec004"
+    //"URI": "mongodb://localhost:27017/comp229_sec004"
+    "URI": "mongodb+srv://admin:admin@mongodbserver.domqeju.mongodb.net/comp229_sec004?retryWrites=true&w=majority"
 }
