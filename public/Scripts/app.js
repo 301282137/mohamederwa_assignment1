22855d3ecc0cf80a0d333cbea3de9388db7d8d5e
Mohamed <mohamederwa.11@gmail.com>
2023-02-17 10:10:13 -0500

    m

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,13 +1,19 @@
-// Mohamed Erwa
-// 301282137
-// COMP229 Sec004
-// 4/2/2023
+// IIFE -- Immediately Invoked Function Expression
+(function () {
 
-(function(){
-
-    function Start()
-    {
+    function Start() {
         console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+
+        for (button of deleteButtons) {
+            button.addEventListener('click', (event) => {
+                if (!confirm("Are you sure?")) {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            });
+        }
     }
 
     window.addEventListener("load", Start);
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,34 +6,45 @@
 var express = require('express');
 var router = express.Router();
 
+let indexController = require('../controllers/index');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home'});
+router.get('/', function (req, res, next) {
+  res.render('home', { title: 'Home' });
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home'});
+router.get('/home', function (req, res, next) {
+  res.render('home', { title: 'Home' });
 });
 
 /* GET About Us page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About'});
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 /* GET Products page. */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+router.get('/projects', function (req, res, next) {
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+router.get('/services', function (req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 
 /* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
+
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
